109cb3a33d478d19dfcf9f2f41b52c38afb0cdd4
Ashwin Prasad <iashwin.prasada@gmail.com>
2019-02-21 11:18:20 +0100

    [Done] Common inteface for all ETSI documented API calls

--- src/adaptor/wrappers/CommonInterface/__init__.py
+++ src/adaptor/wrappers/CommonInterface/__init__.py
@@ -2,3 +2,5 @@ from .auth import CommonInterfaceAuth
 from .vnfpkgm import CommonInterfaceVnfPkgm
 from .nsd import CommonInterfaceNsd
 from .nslcm import CommonInterfaceNslcm
+from .nsfm import CommonInterfaceNsfm
+from .nspm import CommonInterfaceNspm
