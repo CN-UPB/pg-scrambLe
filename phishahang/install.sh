03e4bbab979a150771479765285979d393bc7dc2
Ashwin Prasad <iashwin.prasada@gmail.com>
2019-05-21 21:25:42 +0200

    [Fix] docker-py update file

--- phishahang/install.sh
+++ phishahang/install.sh
@@ -4,7 +4,13 @@ sudo apt-get update
 sudo apt-get install -y ansible
 sudo apt-get install -y git
 git clone --single-branch --branch scramble-pishahang https://github.com/CN-UPB/pg-scrambLe.git
+# git clone https://github.com/CN-UPB/Pishahang.git
 cd pg-scrambLe/phishahang/Pishahang-master/son-install
 mkdir ~/.ssh
 echo sonata | tee ~/.ssh/.vault_pass
-ansible-playbook utils/deploy/sp.yml -e "target=localhost public_ip=<your_ip4_address>" -v
+ansible-playbook utils/deploy/sp.yml -e "target=localhost public_ip=131.234.29.100" -v
+
+
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+sudo dpkg-reconfigure locales
