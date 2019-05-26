473f16a6af6d8b60bb0de88dc70d508799f8f98c
Ashwin Prasad <iashwin.prasada@gmail.com>
2019-05-26 23:30:47 +0200

    [WIP] Scalability instance creation

---
+++ phishahang/Pishahang-master/son-mano-framework/plugins/son-mano-scaling/son_mano_scaling/config.py
@@ -0,0 +1,23 @@
+PARENT_IP = "vm-hadik3r-06.cs.uni-paderborn.de"
+NETDATA_PORT = 19999
+
+DUMMY_INSTANCE_IP = "vm-hadik3r-07.cs.uni-paderborn.de"
+
+
+def run_async(func):
+	"""
+		run_async(func)
+			function decorator, intended to make "func" run in a separate
+			thread (asynchronously).
+			Returns the created Thread object
+	"""
+	from threading import Thread
+	from functools import wraps
+
+	@wraps(func)
+	def async_func(*args, **kwargs):
+		func_hl = Thread(target = func, args = args, kwargs = kwargs)
+		func_hl.start()
+		return func_hl
+
+	return async_func
