8bbce9d08b6bcafad9216578d337ef5cce881425
bhargavi <bhargavimohan.me@gmail.com>
2019-06-05 10:39:42 +0200

    [WIP] OSM experiment runner

--- experiments/experiment-runner/run-experiment-osm.py
+++ experiments/experiment-runner/run-experiment-osm.py
@@ -8,13 +8,24 @@
 
 import wrappers
 import time
-
+nsName = "stress_case1"
+nsDescription = "NSDESCRIPTION"
+nsdId = "stress_case1-ns"
+vimAccountId = "openstack"
 
 start_time = int(time.time())
-time.sleep(10)
+time.sleep(5)
 print (start_time)
 
-ns_inst_time = int (time.time())
+ns_inst_time = int(time.time())
+
+wrappers.OSMClient.nslcm.Nslcm.post_ns_instances_nsinstanceid_instantiate(self, token, nsDescription,
+                                 nsName, nsdId, vimAccountId, host=None, port=None)
+time.sleep(20)
+ns_term_time = int(time.time())                                 
+
+print(ns_inst_time)
+print(ns_term_time)
 
 
 
