904990c8f0fbe35e0b189274f8a4540d2e4fb5f2
ashwinprasadme <iashwin.prasada@gmail.com>
2019-08-05 23:13:45 +0200

    [WIP] kubernetes API

---
+++ experiments/experiment-runner/docker-api.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+import time
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+from pprint import pprint
+
+from kubernetes import client, config
+
+aToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6ImRlZmF1bHQtdG9rZW4tOWo3cTgiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlcnZpY2UtYWNjb3VudC5uYW1lIjoiZGVmYXVsdCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjNjOTIxZjk4LTdhZTMtNDE2NS1hYjlmLWM1ZThiZTM5MjNjYyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpkZWZhdWx0OmRlZmF1bHQifQ.I-cs6gpNfHopqCJEnf9Jhhga9CQhovt--MY90uKlXKvPBhIQ-sdtkXf1_1cXTnmKcayA8p66FRliK61swlENTTZEj62OehfxfQz-7JQfqgp0g9xRae3laCYUyGwkCioLGnqI7sJILau4_2KXLw1nDiwy4gDGIT54q_JBR2Yhxwvz_N6uyJAt1rLdvHdpTKQc8ZnC0LlijgcbnGfXpWv6AO803E0ufUvAr6QpdoxUHO0BJcyEWTgE-xU97tgiX8CR1_QVtI8bGZa33_FhueMsILVt-BFZ5laxcSCYNK275mFX6ILRNcfWKKmKaFG8sC_xD5JWIES_VFpys6gem8qGCQ"
+
+aConfiguration = client.Configuration()
+aConfiguration.host = "https://131.234.29.11"
+
+aConfiguration.verify_ssl = False
+aConfiguration.api_key = {"authorization": "Bearer " + aToken}
+
+aApiClient = client.ApiClient(aConfiguration)
+
+
+def delete_replication_controller():
+
+    v1 = client.CoreV1Api(aApiClient)
+    print("Listing pods with their IPs:")
+    ret = v1.list_namespaced_replication_controller(namespace='default', watch=False)
+    for i in ret.items:
+        # delete_pod('kargo-3','default')
+      print("%s\t%s" %
+            (i.metadata.name, i.metadata.creation_timestamp))
+
+      api_instance = client.CoreV1Api(aApiClient)
+      body = client.V1DeleteOptions()
+      api_response = api_instance.delete_namespaced_replication_controller(i.metadata.name, i.metadata.namespace, body=body)
+
+def delete_replication_pod():
+
+    v1 = client.CoreV1Api(aApiClient)
+    print("Listing pods with their IPs:")
+    ret = v1.list_namespaced_pod(namespace='default', watch=False)
+    for i in ret.items:
+        # delete_pod('kargo-3','default')
+      print("%s\t%s" %
+            (i.metadata.name, i.metadata.creation_timestamp))
+
+      api_instance = client.CoreV1Api(aApiClient)
+      body = client.V1DeleteOptions()
+      api_response = api_instance.delete_namespaced_pod(i.metadata.name, i.metadata.namespace, body=body)
+
+
+
+if __name__ == '__main__':
+    delete_replication_controller()
+    delete_replication_pod()
